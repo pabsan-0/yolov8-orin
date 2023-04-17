2203e183c22d328c9fda52ce7a32cc991aabe3f7
pabsan-0 <psantana@catec.aero>
2023-04-17 15:40:53 +0200

    yolov8 jetson dockerfile plus some automations

--- nvds/pipeline.sh
+++ nvds/pipeline.sh
@@ -1,8 +1,12 @@
-gst-launch-1.0 v4l2src device=/dev/video0                                                     \
+cd `git rev-parse --show-toplevel`
+
+VIDEO_PATH = "$PWD/$1"
+
+gst-launch-1.0 filesrc location="$VIDEO_PATH"                                                 \
     ! videoconvert ! 'video/x-raw,format=I420'                                                \
     ! queue leaky=2 max-size-buffers=100 max-size-time=0 max-size-bytes=1                     \
     ! nvvideoconvert ! 'video/x-raw(memory:NVMM),format=(string)I420,width=640,height=480'    \
-    ! m.sink_0 nvstreammux name=m batch-size=1 width=1280 height=1280 nvbuf-memory-type=0     \
+    ! m.sink_0 nvstreammux name=m batch-size=1 width=480 height=480 nvbuf-memory-type=0       \
     ! nvvideoconvert ! nvinfer config-file-path="$PWD/nvds/coco_config_infer_primary.txt"     \
     ! nvmultistreamtiler width=640 height=480                                                 \
-    ! nvvideoconvert ! nvdsosd ! nveglglessink async=0 sync=0                                 ;
+    ! nvvideoconvert ! nvdsosd ! filesink location=out.mp4                                    ;
