6224d4d657122326127eb1cb7fa7f344146b3102
pabsan-0 <psantana@catec.aero>
2023-04-19 09:41:31 +0200

    mostly docs and aftertest changes

--- nvds/pipeline.sh
+++ nvds/pipeline.sh
@@ -1,12 +1,12 @@
-cd `git rev-parse --show-toplevel`
+#! /bin/bash
 
-VIDEO_PATH = "$PWD/$1"
+VIDEO_PATH="$PWD/yolo/media/videoplayback.mp4"
 
-gst-launch-1.0 filesrc location="$VIDEO_PATH"                                                 \
-    ! videoconvert ! 'video/x-raw,format=I420'                                                \
+gst-launch-1.0 -ve filesrc location="$VIDEO_PATH"                                             \
+    ! qtdemux ! av1dec                                                                        \
     ! queue leaky=2 max-size-buffers=100 max-size-time=0 max-size-bytes=1                     \
-    ! nvvideoconvert ! 'video/x-raw(memory:NVMM),format=(string)I420,width=640,height=480'    \
-    ! m.sink_0 nvstreammux name=m batch-size=1 width=480 height=480 nvbuf-memory-type=0       \
+    ! nvvideoconvert ! 'video/x-raw,format=I420'                                              \
+    ! nvvideoconvert ! 'video/x-raw(memory:NVMM)'                                             \
+    ! m.sink_0 nvstreammux name=m batch-size=1 width=640 height=640 nvbuf-memory-type=0       \
     ! nvvideoconvert ! nvinfer config-file-path="$PWD/nvds/coco_config_infer_primary.txt"     \
-    ! nvmultistreamtiler width=640 height=480                                                 \
     ! nvvideoconvert ! nvdsosd ! filesink location=out.mp4                                    ;
